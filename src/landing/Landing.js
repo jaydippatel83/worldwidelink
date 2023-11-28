facac001fd078d70dced0e8c5fa352225cce9c3c
jaydippatel83 <jaydippatel83.pj@gmail.com>
2023-11-28 14:38:23 +0530

    remove unused sections

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -81,9 +81,11 @@ function Landing() {
                             <div className="main-bnr style-1">
                                 <div className="container">
                                     <div className="row align-items-center">
-                                        <div className="col-12 text-center">
-                                            <h1 className="" >Your Globel OTC desk for <br />Cryptocurrencies</h1>
-                                            <p className="text text-primary " >Transfer USD, EUR, or Crypto and start trading today!</p>
+                                        <div className="col-8 text-center mx-auto">
+                                            <h1 className="" >Cross-Chain DeFi Hub</h1>
+                                            <p className="text text-white " >CCIP-powered DeFi platform offering cross-chain
+                                                integration of lending protocols, escrow, staking,
+                                                token transfers, and SOS alerts.</p>
                                             <button onClick={address ? handleNavigate : connectWallet} className="btn space-lg  btn-shadow btn-primary " >Launch App</button>
 
                                             {/* <ul className="image-before">
@@ -98,7 +100,7 @@ function Landing() {
                                 <img className="bg-shape3" src={Shape3} alt="" />
                                 <img className="bg-shape4" src={Shape3} alt="" />
                             </div>
-                            <div className="clearfix bg-primary-light">
+                            <div className="clearfix bg-primary-light pb-5">
                                 <div className="container">
                                     <div className="currancy-wrapper">
                                         <div className="row justify-content-center">
@@ -110,122 +112,6 @@ function Landing() {
                                     </div>
                                 </div>
                             </div>
-                            <section className="clearfix section-wrapper1 bg-primary-light">
-                                <div className="container">
-                                    <div className="content-inner-1">
-                                        <div className="section-head text-center">
-                                            <h2 className="title">Why Trust Us?</h2>
-                                            <p>Trust comes from experience. Many of the pleased customers may function as a guide for you.</p>
-                                        </div>
-                                        <div className="row">
-                                            {trustBlog.map((data, ind) => (
-                                                <div className="col-lg-6 m-b30" key={ind}>
-                                                    <div className="icon-bx-wraper style-2">
-                                                        <div className="icon-media">
-                                                            <img src={data.image} alt="" />
-                                                        </div>
-                                                        <div className="icon-content">
-                                                            <h4 className="title">{data.title}</h4>
-                                                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
-                                                            <Link className="btn btn-primary btn-gradient btn-shadow" to={"/about-us"}>Read More</Link>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            ))}
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="container">
-                                    <div className="form-wrapper-box style-1 text-center">
-                                        <div className="section-head " >
-                                            <h4 className="title m-t0">How to Purchase from us ?</h4>
-                                            <p>Fill out the below form and we will contact you via email & details</p>
-                                        </div>
-                                        <form className="dz-form" onSubmit={(e) => formDetails(e)}>
-                                            <div className="form-wrapper">
-                                                <div className="flex-1">
-                                                    <div className="row g-3">
-                                                        <div className="col-xl-3 col-md-6 " >
-                                                            <input name="dzName" type="text" required="" placeholder="Wallet Address" className="form-control" />
-                                                        </div>
-                                                        <div className="col-xl-3 col-md-6 " >
-                                                            <Dropdown className="select-drop">
-                                                                <Dropdown.Toggle as="div" className="i-false select-drop-toggle">
-                                                                    <img src={selecttext[0]} alt="" /> {selecttext[1]} <i className="fa-sharp fa-solid fa-angle-down" />
-                                                                </Dropdown.Toggle>
-                                                                <Dropdown.Menu>
-                                                                    <Dropdown.Item onClick={() => setSelectText([coin4, "Bitcoin"])}><img src={coin4} alt="" /> Bitcoin</Dropdown.Item>
-                                                                    <Dropdown.Item onClick={() => setSelectText([coin3, "Ethereum"])}><img src={coin3} alt="" /> Ethereum</Dropdown.Item>
-                                                                    <Dropdown.Item onClick={() => setSelectText([coin1, "Tether"])}><img src={coin1} alt="" /> Tether</Dropdown.Item>
-                                                                </Dropdown.Menu>
-
-                                                            </Dropdown>
-                                                        </div>
-                                                        <div className="col-xl-3 col-md-6 " >
-                                                            <input name="dzName" type="text" required="" placeholder="How much worth in $?" className="form-control" />
-                                                        </div>
-                                                        <div className="col-xl-3 col-md-6 ">
-                                                            <input name="dzName" type="text" required="" placeholder="Email Address" className="form-control" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <button type="submit" className="btn btn-lg btn-gradient btn-primary btn-shadow">Get Strated</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                                <img className="bg-shape1" src={Shape1} alt="" />
-                            </section>
-                            <section className="content-inner bg-light icon-section section-wrapper2">
-                                <div className="container">
-                                    <div className="section-head text-center">
-                                        <h2 className="title">One-stop solution to buy and sell <span className="text-primary"> cryptocurrency </span> with Cash</h2>
-                                    </div>
-                                    <div className="row sp60">
-                                        <OneStop />
-                                    </div>
-                                </div>
-                                <img className="bg-shape1" src={Shape1} alt="" />
-                            </section>
-                            <section className="content-inner bg-white blog-wrapper">
-                                <img className="bg-shape1" src={Shape1} alt="" />
-
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-xl-7 col-lg-12">
-                                            <div className="section-head " >
-                                                <h6 className="sub-title text-primary">FROM OUR BLOG</h6>
-                                                <h2 className="title">Recent News &amp; Updates</h2>
-                                            </div>
-                                            <RecentNews />
-                                        </div>
-                                        <div className="col-xl-5 col-lg-12 m-b30 " >
-                                            <div className="dz-card style-2" style={{ backgroundImage: "url(" + bloglg + ")" }}>
-                                                <div className="dz-category">
-                                                    <ul className="dz-badge-list">
-                                                        <li><Link to={"#"} className="dz-badge">14 Fan 2022</Link></li>
-                                                    </ul>
-                                                </div>
-                                                <div className="dz-info">
-                                                    <h2 className="dz-title"><Link to={"/blog-details"} className="text-white">Directly support individuals Crypto</Link></h2>
-                                                    <div className="dz-meta">
-                                                        <ul>
-                                                            <li className="post-author">
-                                                                <Link to={"#"}>
-                                                                    <img src={avatar3} alt="" className="me-2" />
-                                                                    <span>By Noare</span>
-                                                                </Link>
-                                                            </li>
-                                                            <li className="post-date"><Link to={"#"}> 12 May 2022</Link></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </section>
                         </div>
                         <Footer />
                     </>

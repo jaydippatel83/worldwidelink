64f691c0e9d24a6bd415911b40f1c101251018fa
jaydippatel83 <jaydippatel83.pj@gmail.com>
2023-12-05 18:57:26 +0530

    changes in airdrop

--- src/jsx/components/Dashboard/Home.js
+++ src/jsx/components/Dashboard/Home.js
@@ -13,16 +13,18 @@ import TxTable from './widgets/TxTable';
 import Converter from './widgets/Converter';
 import EscrowTable from './widgets/EscrowTable';
 import Airdrop from './widgets/Airdrop';
+import { Web3Context } from '../../../context/Web3Context';
 const Home = () => {
 	const [isCssLoaded, setIsCssLoaded] = useState(false);
 	const { changeBackground } = useContext(ThemeContext);
+	const web3Context = useContext(Web3Context);
+	const { address } = web3Context;
 	useEffect(() => {
 		changeBackground({ value: "light", label: "Light" });
 		setTimeout(() => {
 			setIsCssLoaded(true);
 		}, 1000);
 	}, []);
-
 	return (
 		<>
 			{
@@ -39,7 +41,9 @@ const Home = () => {
 						{/* <TxTable /> */}
 						<MarketInfo />
 						<Converter />
-						<Airdrop />
+						{
+							address === process.env.REACT_APP_ADMIN && <Airdrop />
+						}
 					</div>
 			}
 		</>
--- src/jsx/components/Escrow/CreateAgreement.js
+++ src/jsx/components/Escrow/CreateAgreement.js
@@ -54,7 +54,6 @@ export default function CreateAgreement() {
 
             setLoading(false);
             alert('Funds deposited successfully.');
-            console.log('----');
         } catch (error) {
             console.error("Transaction failed:", error.message);
             setLoading(false);
@@ -157,7 +156,7 @@ export default function CreateAgreement() {
                                             // onClick={submitWork}
                                             // onClick={releaseFund}
                                             onClick={createAgreement}
-                                            // onClick={fetchAllAgreements}
+                                        // onClick={fetchAllAgreements}
 
                                         >Create escrow</button>
                                     </div>
